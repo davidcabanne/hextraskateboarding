e18ae11e3d14988dee2b9fd9eecf6f3a8688913f
davidcabanne <davidcbne@gmail.com>
2023-12-25 23:46:31 +0100

    fix: section flicker on iOS

--- components/Section/index.jsx
+++ components/Section/index.jsx
@@ -10,7 +10,6 @@ const Container = styled.section`
   width: 100%;
   height: 100%;
   min-height: 100vh;
-  min-height: 100svh;
   display: flex;
   justify-content: center;
   align-items: center;
